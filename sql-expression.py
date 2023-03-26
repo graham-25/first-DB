f4778807d5baca890b91a9358f76ace670ca2201
graham-25 <grbills29@googlemail.com>
2023-03-26 22:45:46 +0000

    ORM start

--- sql-expression.py
+++ sql-expression.py
@@ -40,10 +40,10 @@ track_table = Table(
 with db.connect() as connection:
     
     # Query 1 - select all records from the "Artist" table
-    # select_query = artist_table.select()
+    select_query = artist_table.select()
 
     # Query 2 - select only the "Name" column from the "Artist" table
-    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+    # select_query = artist_table.select().with_only_columns(artist_table.c.Name)
 
     # Query 3 - select only 'Queen' from the "Artist" table
     # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
@@ -55,9 +55,10 @@ with db.connect() as connection:
     # select_query = album_table.select().where(album_table.c.ArtistId == 51)
 
     # Query 6 - select all tracks where the composer is 'Queen' from the "Track" table
-    select_query = track_table.select().where(track_table.c.Composer == "Queen")
+    # select_query = track_table.select().where(track_table.c.Composer == "Queen")
 
     results = connection.execute(select_query)
     for result in results:
         print(result)
 
+    
